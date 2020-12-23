231fe229197d2609a4e92d7cc3b34d45a7c17f36
yuriiihor <hefestsbunker@gmail.com>
2020-12-23 04:20:39 +0200

    Added minor unit test

---
+++ TaxInspection_Tests/MainWindow_Test.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TaxInspection_Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TaxInspection;
+
+    [TestClass]
+    public class MainWindow_Test
+    {
+        [TestMethod]
+        public void MainWindow_TestHeight()
+        {
+            // Arrange
+            int expectedHeight = 450;
+
+            // Act
+            MainWindow window = new MainWindow();
+            
+            // Assert
+            Assert.AreEqual(expectedHeight, window.Height);
+        }
+    }
+}
